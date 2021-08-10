59455d84e7078e8553337314d1760813c06c0d4d
Brendan <brendanhawk03@aol.com>
2021-08-10 10:10:21 -0400

    Final version

--- js/site.js
+++ js/site.js
@@ -1,4 +1,44 @@
-// Call Hello World
-function HelloWorld() {
-    alert("Hello World");
+//Get the string from the page
+//Controller function
+function getValues() {
+
+    document.getElementById("alert").classList.add("invisible");
+
+    //Get string from page
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+
+    displayString(revString);
+
+}
+
+//Reverse the string
+//Logic function
+function reverseString(userString) {
+
+    let revString = [];
+
+    //let name = Bobby
+    //name[0] = "B";
+    // name[4] = "y";
+    // last position in an array name.length - 1;
+    
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+
+        revString += userString[index];
+        
+    }
+    return revString;
+}
+
+//Display the reversed string to the user
+//View function
+function displayString(revString) {
+
+    //write the message to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`; 
+
+    //turn on the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
